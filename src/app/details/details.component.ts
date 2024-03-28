6e250d2c3c6cc360703508649337b332c76be1e9
Meelenych <meelenych@gmail.com>
2024-03-28 11:07:42 -0600

    components update to use asynchronous json server

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -50,8 +50,10 @@ export class DetailsComponent {
   housingLocationId = -1;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   applyForm = new FormGroup({
